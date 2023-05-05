88815c6d41e020533e5c17cd64f41af0e787fe91
Mario Ajenandro Muñoz <79519578+mariodemoder@users.noreply.github.com>
2023-05-05 15:53:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ const fondo = document.querySelector('#fondo');
 (()=>{
     BtnBotones.addEventListener('click', agregarBotones);
     botones.addEventListener('click', delegacion);
-    
+}
+)();
+
+(()=>{
+    const color = localStorage.getItem('colorFondo');
+    if (color == null)
+    {
+        fondo.className = 'bg-dark';
+    } else {
+        fondo.className = color;
+    }
 }
 )();
 
@@ -17,20 +27,27 @@ function delegacion(e){
 switch (colorBoton) {
     case 'btn-primary':
         fondo.className = 'bg-primary'
+        localStorage.setItem('colorFondo', "bg-primary");
     break;
     case 'btn-secondary':
         fondo.className = 'bg-secondary'
+        localStorage.setItem('colorFondo', "bg-secondary");
     break;
     case 'btn-danger':
         fondo.className = 'bg-danger'
+        localStorage.setItem('colorFondo', "bg-danger");
         break;
     case 'btn-warning':
         fondo.className = 'bg-warning'
+        localStorage.setItem('colorFondo', "bg-warning");
         break;
     case 'btn-success':
         fondo.className = 'bg-success'
+        localStorage.setItem('colorFondo', "bg-success");
         break;
 }
+
+
 }
 function agregarBotones(e){
     e.preventDefault();
